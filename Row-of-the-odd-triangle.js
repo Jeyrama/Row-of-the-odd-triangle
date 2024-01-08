9ebbf1fb135dc2f2ba58abd0a155f2fd3935b684
Jey <126204426+Jeyrama@users.noreply.github.com>
2024-01-08 13:17:02 -0800

    Update Row-of-the-odd-triangle.js

--- Row-of-the-odd-triangle.js
+++ Row-of-the-odd-triangle.js
@@ -22,3 +22,13 @@ function oddRow(n) {
 }
 
 // or 
+
+/* EXPLANATION:
+1. Summ of all odd numbers in n-row is equal to: n*n*n,
+2. It can be described also as: x+(x+2)+(x+3)+(x+4), where "x" is first odd number in the row
+3. By using arithetic sequence formula [Sn = ( (2*a1 + (N-1) * r) / 2) * N],
+    it can be described as: n*x + n*n - n
+4. By comparing two patters, we receive: n*n*n = n*x + n*n - n
+5. By extracting "x", we can calculate first number in the odd row: x = n*n - n + 1     
+*/
+const oddRow=(n)=>  new Array(n).fill(n*n - n + 1).map((el,i)=> i? el + i*2: el)
